5d00b46a1a588e830fbc141c363e214381b40dcc
grlukasa <88y57U9011>
2021-11-27 14:31:58 +0100

    Initial commit

---
+++ src/Exam.java
@@ -0,0 +1,8 @@
+public class Exam {
+    public static void main(String[] args) {
+        String name;
+        name = "Grzegorz";
+
+        System.out.println(name + " zdał końcowy egzamin podyplomowy.");
+    }
+}
